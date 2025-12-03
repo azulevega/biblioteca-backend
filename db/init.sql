49677327707b481708e53a5afe6396cdbeeb847a
Azul Valentina Vega Mena <azulvalentinavegamena@gmail.com>
2025-12-03 09:12:23 -0600

    archivos creados

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (255) NOT NULL,
+    author VARCHAR (255) NOT NULL,
+    year INT NOT NULL,
+    available_copies INT NOT NULL
+);
